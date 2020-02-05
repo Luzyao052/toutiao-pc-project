8564c96b19f6f91bea68d5f08a8540a2ae726ed9
Your Name <you@example.com>
2020-02-05 14:40:32 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
+// 导入包（包括第三方 | 自己的）
 import Vue from 'vue'
 import App from './App.vue'
 
+// 配置vue框架的日志级别  productionTip
+// false 开发环境级别的日志，日志详细
+// true 生产环境级别的日志，上线之后部署服务器之后，日志简单
 Vue.config.productionTip = false
 
+// 根实例
 new Vue({
   render: h => h(App)
 }).$mount('#app')
+
+// main.js的职责
+// 1.项目依赖需要的资源（公共功能模块，需要导入main.js）
+// 2.创建一个vue根实例
+
