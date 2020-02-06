1ae02a40ef03a4c915d77e4e2bc9848892d08ef6
Your Name <you@example.com>
2020-02-06 11:00:06 +0800

    登录模块-路由规则和组件准备

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import VueRouter from 'vue-router'
 import Vue from 'vue'
 Vue.use(VueRouter)
+
+//导入
+import Login from '@/views/login'
 export default new VueRouter({
-  routes: [] // 路由规则
+  routes: [
+    { path: '/login', component: Login }
+  ] // 路由规则
 })
---
+++ src/views/login/index.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+  loginlogin
+</div>
+</template>
+
+<script>
+export default {
+name:'app-login'
+}
+</script>
+
+<style scoped>
+</style>
