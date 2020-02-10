07d0beb39fdd74818124576bd1d5d54c75d21b35
Your Name <you@example.com>
2020-02-10 15:00:07 +0800

    内容管理-axios的请求拦截器

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,17 @@ import auth from '@/utils/auth.js'
 // 基准地址配置
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
 // 请求头token配置
-axios.defaults.headers.Authorization = `Bearer ${auth.getUser().token}`
+// axios.defaults.headers.Authorization = `Bearer ${auth.getUser().token}`
+// 添加请求拦截器
+axios.interceptors.request.use(function (config) {
+  // 在发送请求之前做些什么
+  // 获取本地的token，如果有token咱们就追加即可
+  const user = auth.getUser()
+  if (user.token) config.headers.Authorization = `Bearer ${user.token}`
+  return config;
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error);
+});
 
 export default axios
