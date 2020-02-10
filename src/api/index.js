b45214117e09b83e19c5cc5ce18c67d8b7e1ebe8
Your Name <you@example.com>
2020-02-10 15:53:53 +0800

    内容管理-axios的响应拦截器

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import auth from '@/utils/auth.js'
+import router from '@/router'
 // 基准地址配置
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
 // 请求头token配置
@@ -13,6 +14,24 @@ axios.interceptors.request.use(function (config) {
   return config;
 }, function (error) {
   // 对请求错误做些什么
+  return Promise.reject(error);
+});
+
+// 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+  // 对响应数据做点什么
+  return response;
+}, function (error) {
+  // 对响应错误做点什么
+  if (error.response && error.response.status === 401) {
+    // 拦截到登录
+    // 大家代码：this.$router.push('/login')
+    // this 一定不是vue实例，只有vue实例才能调用 $router或者$route
+    // $router 就是路由实例，我们在router/index.js 初始化过路由实例
+    // $router === router/index.js  理解吗？
+    // window.location.href|hash 这样的跳转，可能不会触发路由的相关功能
+    router.push('/login')
+  }
   return Promise.reject(error);
 });
 
