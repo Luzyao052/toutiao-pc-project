4bf573f591c2f36ba9584419d6bac9ca5a98e6d4
Your Name <you@example.com>
2020-02-15 20:09:27 +0800

    发布文章-封面组件-基础结构

--- src/plugin.js
+++ src/plugin.js
@@ -1,6 +1,7 @@
 // 基于vue的插件
 import MyBread from '@/components/my-bread'
 import MyChannel from '@/components/my-channel'
+import MyImage from '@/components/my-image'
 export default {
   // 在main.js使用Vue.use(当前插件)传入Vue参数
   install (Vue) {
@@ -9,5 +10,7 @@ export default {
     // Vue.component('AAA', MyBread)
     //频道组件
     Vue.component(MyChannel.name, MyChannel)
+    //封面组件
+    Vue.component(MyImage.name, MyImage)
   }
 }
