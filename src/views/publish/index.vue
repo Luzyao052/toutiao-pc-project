02167144491987b895114229c103e30a8cdb9bf0
Your Name <you@example.com>
2020-02-15 19:55:52 +0800

    发布文章-富文本的使用与调整

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class='container-publish'>
+  <div class="container-publish">
     <el-card>
       <div slot="header">
         <my-bread>发布文章</my-bread>
@@ -8,7 +8,9 @@
         <el-form-item label="标题：">
           <el-input v-model="articleForm.title" style="width:400px"></el-input>
         </el-form-item>
-        <el-form-item label="内容：">富文本</el-form-item>
+        <el-form-item label="内容：">
+          <quill-editor v-model="articleForm.content" :options="editorOption"></quill-editor>
+        </el-form-item>
         <el-form-item label="封面：">封面组件</el-form-item>
         <el-form-item label="频道：">
           <my-channel v-model="articleForm.channel_id"></my-channel>
@@ -20,18 +22,42 @@
 </template>
 
 <script>
+// 引入局部富文本
+import "quill/dist/quill.core.css";
+import "quill/dist/quill.snow.css";
+import "quill/dist/quill.bubble.css";
+import { quillEditor } from "vue-quill-editor";
 export default {
-  name: 'app-publish',
+  name: "app-publish",
+  components: {
+    quillEditor
+  },
   data() {
     return {
-      articleForm:{
+      articleForm: {
         // 文章数据
-        title: '',
-        channel_id: null
+        title: "",
+        channel_id: null,
+        content: null
+      },
+      // 富文本配置对象
+      editorOption: {
+        // 模块配置
+        modules: {
+          toolbar: [
+            ["bold", "italic", "underline", "strike"], // toggled buttons
+            ["blockquote", "code-block"],
+            [{ header: 1 }, { header: 2 }], // custom button values
+            [{ list: "ordered" }, { list: "bullet" }],
+            [{ script: "sub" }, { script: "super" }], // superscript/subscript
+            [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+            ["image"]
+          ]
+        }
       }
-    }
+    };
   }
-}
+};
 </script>
 
 <style scoped lang='less'></style>
