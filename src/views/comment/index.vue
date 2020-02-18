a51d4cae3a06d55eb4a919504944e5ce9f71848f
Your Name <you@example.com>
2020-02-18 17:31:40 +0800

    评论管理-切换状态

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="container-comment">
     <el-card>
       <div slot="header">
         <my-bread>评论管理</my-bread>
@@ -13,8 +13,13 @@
         </el-table-column>
         <el-table-column label="操作">
           <template slot-scope="scope">
-            <el-button size="small" v-if="scope.row.comment_status" type="danger">关闭评论</el-button>
-            <el-button size="small" v-else type="success">打开评论</el-button>
+            <el-button
+              @click="toggleStatus(scope.row)"
+              size="small"
+              v-if="scope.row.comment_status"
+              type="danger"
+            >关闭评论</el-button>
+            <el-button @click="toggleStatus(scope.row)" size="small" v-else type="success">打开评论</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -51,10 +56,47 @@ export default {
     this.getComments();
   },
   methods: {
+    // 切换状态
+    toggleStatus(row) {
+      // 解释：row 其实就是comments数组遍历后的每一个对象。
+      // 请求：put comments/status
+      // 键值对传参：?article_id=文章ID
+      // 请求体传参：allow_comment=想修改的状态  真 评论   假 不能
+      // 关闭打开提示不一样：
+      // 关闭：您是否确定要关闭该文章的评论功能，关闭后用户无法对文章进行评论。
+      // 打开：您是否确定要打开该文章的评论功能。
+      // row 中有 comment_status 标识当前文章状态
+      const text1 = "确定要关闭评论嘛，关闭后将无法进行相关评论";
+      const text2 = "确定要打开评论嘛";
+      this.$confirm(row.comment_status ? text1 : text2, {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(async () => {
+          try {
+            // res.data.data.allow_comment 修改后的状态
+            const res = await this.$http.put(
+              `comments/status?article_id=${row.id}`,
+              { allow_comment: !row.comment_status }
+            );
+            // console.log(res);
+            this.$message.success(
+              // 提示
+              res.data.data.allow_comment ? "打开成功" : "关闭成功"
+            );
+            // 修改当前行文章的评论状态
+            row.comment_status = res.data.data.allow_comment;
+          } catch (e) {
+            this.$message.error("操作失败");
+          }
+        })
+        .catch(() => {});
+    },
     // 分页
     pager(newPage) {
-     this.reqParams.page = newPage
-     this.getComments()
+      this.reqParams.page = newPage;
+      this.getComments();
     },
     // 获取
     async getComments() {
